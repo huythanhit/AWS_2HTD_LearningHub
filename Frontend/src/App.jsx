e315e6a0879e55ba4a6dfa74c85e4bce58de4a79
HtmDuyn <hg2922004@gmail.com>
2025-11-16 17:59:31 +0700

    setup folders and tailwind
    Added initial project structure for the frontend, including index.html, main entry points, and test placeholders for components, contexts, hooks, pages, routes, services, and types. Integrated Tailwind CSS and PostCSS configuration, updated package.json dependencies, and created index.css for styling.

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+function App() {
+  return (
+    <div className="text-4xl font-bold text-red-500 p-8">
+      Tailwind đã hoạt động!
+    </div>
+  )
+}
+export default App
